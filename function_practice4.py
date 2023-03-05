023a0b901d60bc15cd93020577f431b13d7129c2
Allison Lowe <alliisonlowe@gmail.com>
2023-03-04 18:31:19 -0800

    Finish assignment

---
+++ function_practice4.py
@@ -0,0 +1,79 @@
+#Activity: Python Function Practice Part 4
+#Function 1
+#Write a Python function called max_num()to find the maximum of three numbers.
+def max_num(int1, int2, int3):
+    return max([int1, int2, int3])
+
+print(f"Max_Num Function")
+print(max_num(1, 2, 3))
+print(max_num(100, 33, 2))
+    
+#Function 2
+#Write a Python function called mult_list() to multiply all the numbers in a list.
+def mult_list(lst):
+    if len(lst) == 0:
+        return 0
+    prod = lst[0]
+    if len(lst) >1:
+        for i in lst[1:]:
+            prod = prod * i
+    return prod
+
+print(f"\nMulti_List Function:")
+print(mult_list([1,2,3]))
+print(mult_list([]))
+
+#Function 3
+#Write a Python function called rev_string() to reverse a string.
+def rev_string(str):
+    return str[::-1]
+
+print(f"\nRev_String Function")
+print(rev_string(""))
+print(rev_string("mango"))
+print(rev_string("Allison Lowe"))
+
+#Function 4
+#Write a Python function called num_within() to check whether a number falls in a given range.
+def num_within(num, beg, end):
+    if num >= beg and num <= end:
+        return True
+    else:
+        return False
+
+print(f"\nNum_Within Function")
+print(num_within(3,2,4))
+print(num_within(3,1,3))
+print(num_within(10, 2, 5))
+
+
+#Function 5
+#Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+triangle = [[1],[1,1]]
+def pascal(n):
+    if n < 1:
+       print("Invalid Number of Rows")
+    if n == 1:
+        return print(triangle[0])
+    else:
+        row_number = 2
+        while len(triangle) < n:
+            row = []
+            row_prev = triangle[row_number-1]
+            length = len(row_prev)+1
+
+            for i in range(length):
+                if i ==0:
+                    row.append(1)
+                elif i > 0 and i < length-1:
+                    row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+                else:
+                    row.append(1)
+            triangle.append(row)
+            row_number+=1
+        
+        for row in triangle:
+            print(row)
+
+pascal(2)
+pascal(5)
